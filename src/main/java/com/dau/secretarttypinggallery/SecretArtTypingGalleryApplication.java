0f924e91a92845e4d475332008a6288ed6d3c15f
BH946 <kobh2895@gmail.com>
2023-08-02 21:48:19 +0900

    add : 프로젝트 추가

---
+++ src/main/java/com/dau/secretarttypinggallery/SecretArtTypingGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.dau.secretarttypinggallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretArtTypingGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretArtTypingGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dau/secretarttypinggallery/SecretArtTypingGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dau.secretarttypinggallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretArtTypingGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
