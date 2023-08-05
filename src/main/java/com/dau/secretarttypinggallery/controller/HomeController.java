cad97cb5fe7922e67958c6d8512f745bdfd98d7d
BH946 <kobh2895@gmail.com>
2023-08-06 01:29:23 +0900

    add : Spring으로 실행하기 위해 간단히 컨트롤러 작성

---
+++ src/main/java/com/dau/secretarttypinggallery/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.dau.secretarttypinggallery.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    // default
+    @GetMapping("home") // URL 매핑(GET)
+    public String home() {
+        return "home"; // home.html 반환
+    }
+
+}
