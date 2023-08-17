c128fcc47e3cab84e77346e093404cdbacb44f45
BH946 <kobh2895@gmail.com>
2023-08-17 17:07:49 +0900

    fix : mysql로 전환 및 배포 준비

--- src/main/java/com/dau/secretarttypinggallery/SecretArtTypingGalleryApplication.java
+++ src/main/java/com/dau/secretarttypinggallery/SecretArtTypingGalleryApplication.java
@@ -3,8 +3,8 @@ package com.dau.secretarttypinggallery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-
 @SpringBootApplication
+
 @EnableCaching // Spring Boot Cache 사용을 선언
 public class SecretArtTypingGalleryApplication {
 
