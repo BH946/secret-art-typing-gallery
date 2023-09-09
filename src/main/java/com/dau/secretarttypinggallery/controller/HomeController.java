01abce8ac5d20a71356ef5b7bccdc61f5f061a58
BH946 <kobh2895@gmail.com>
2023-09-09 18:03:17 +0900

    fix : html 수정작업중... 특히 header 수정완료

---
+++ src/main/java/com/dau/secretarttypinggallery/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.dau.secretarttypinggallery.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // index.html 반환
+    }
+}
