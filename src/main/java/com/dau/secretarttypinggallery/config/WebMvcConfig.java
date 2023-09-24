23a1fd31ea11306f64f47490cd4925efbdb40996
BH946 <kobh2895@gmail.com>
2023-09-25 03:44:22 +0900

    add : index.html 개발 완료

--- src/main/java/com/dau/secretarttypinggallery/config/WebMvcConfig.java
+++ src/main/java/com/dau/secretarttypinggallery/config/WebMvcConfig.java
@@ -2,8 +2,11 @@ package com.dau.secretarttypinggallery.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -12,9 +15,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        CacheControl cacheControl = CacheControl.maxAge(Duration.ofDays(365));
         registry.addResourceHandler("/image/**")
 //                .addResourceLocations("file:///C:/images-spring/");
 //                .addResourceLocations("file:///var/www/images-spring/");
                 .addResourceLocations("file:///"+source.getMyDataSource().getImgPath());
+
+        registry.addResourceHandler("/**") // **/*.*, /resources/**
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(cacheControl); // 정적 리소스들 캐시 추가
     }
 }
